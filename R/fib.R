b9b933fcdfd82e17153e93bb3fe9c6d61d91d149
Robin Stephenson <robin.stephenson@cancer.org.uk>
2019-09-13 09:59:57 +0100

    use sapply rather than purrr for the unit test

--- R/fib.R
+++ R/fib.R
@@ -1,12 +1,9 @@
-# Implement the Fibonacci sequence
+# return the nth element of the Fibonacci sequence
 fib <- function(n) {
   return(as.integer(n))
 }
 
 # Unit tests
-# NB if you see "Error: Can't coerce element [n] from a double to a integer"
-# then coerce your return value to an integer: return(as.integer(n))
-library(purrr)
-(actual <- (map(1:10, fib) %>% flatten_int()))
+(actual <- sapply(1:10, fib))
 (expected <- c(1, 1, 2, 3, 5, 8, 13, 21, 34, 55))
 all.equal(actual, expected)
